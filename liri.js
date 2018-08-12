804b5b716ec80c4904e1c1d57dfdd5a573889b75
Joseph Venuto <jbvenuto@gmail.com>
2018-08-12 13:54:34 -0400

    app now takes in user arguments and stores them

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
-//Include NPM packages for Request, DotEnv, Twitter, Spotify, and OMDB
+//Include NPM packages for Request, DotEnv, Twitter, and Spotify
 var request = require("request");
 require("dotenv").config();
+var twitter = require("twitter");
+var spotify = require("node-spotify-api");
+
+//This variable takes in the arguments from the command line
+nodeArg = process.argv;
+
+//This variable takes an argument from the command line and tells the app which website to search 
+var call = nodeArg[2];
+console.log(call);
+//This variable takes an argument from the command line and tells the app the search term
+var search = "";
+
+//Loop through user arguments to find the search term
+for (i = 3; i < process.argv.length; i++) {
+    console.log(process.argv[i]);
+    search += process.argv[i];
+    console.log(search);
+}
