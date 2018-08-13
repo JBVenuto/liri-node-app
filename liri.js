671e7e8642d0d14f13106cca65f2663b782afdaf
Joseph Venuto <jbvenuto@gmail.com>
2018-08-12 21:50:08 -0400

    app retrieves tweets from my testing twitter

--- liri.js
+++ liri.js
@@ -1,8 +1,13 @@
 //Include NPM packages for Request, DotEnv, Twitter, and Spotify
 var request = require("request");
 require("dotenv").config();
-var twitter = require("twitter");
-var spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+
+//Access keys information
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 //This variable takes in the arguments from the command line
 nodeArg = process.argv;
@@ -24,6 +29,18 @@ console.log(search);
 
 //This is what will happen when a call is made for Twitter
 if (call == "my-tweets") {
+    var params = {screen_name: 'venuto_joseph'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        // console.log(tweets);
+        var tweetArr = tweets;
+        // console.log(tweetArr);
+        for (j = 0; j < tweetArr.length; j++) {
+            console.log(tweetArr[j].user.name + ":");
+            console.log(tweetArr[j].text);
+        }
+    }
+});
 }
 
 //This is what will happen when a call is made for a music search
@@ -34,7 +51,12 @@ else if (call == "spotify-this-song") {
 else if (call == "movie-this") {
 }
 
+//This is what will happen when a call is made to the random file
+else if (call =="do-what-it-says"){
+
+}
+
 //This is what will happen if the user tries to call something the app can't recognize
 else {
-    console.log("Please type 'my-tweets' to see my tweets, 'spotify-this-song' to learn about a song of your choosing, or 'movie-this' to learn about a movie.");
+    console.log("Please type 'my-tweets' to see my tweets, 'spotify-this-song' to learn about a song of your choosing, 'movie-this' to learn about a movie, or 'do-what-it-says' for a random result.");
 };
